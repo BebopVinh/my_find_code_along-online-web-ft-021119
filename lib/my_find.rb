546705768fcbd42e85d241d836719bbf5f23dc87
Vinh Do <vinhdo07@gmail.com>
2019-02-11 22:46:05 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,6 +4,11 @@ def my_find(collection)
   i = 0
   while i < collection.length
     return collection[i] if yield(collection[i])
+<<<<<<< HEAD
     i += 1
   end
 end
+=======
+  end
+end
+>>>>>>> 036f515be539b256747979ff9cd5ba01ed605b83
